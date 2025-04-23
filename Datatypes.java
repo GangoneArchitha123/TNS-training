544f1889af863bbc480a2ad95d62760278b83e57
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-04-23 13:02:59 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,10 @@
+public class Datatypes {
+    public static void main(String []args){
+        boolean a =true;
+        short b =16785;
+        short num=20;
+        int num1=num;
+        System.out.print(num1);
+    }
+    
+}
