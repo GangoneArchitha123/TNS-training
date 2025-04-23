c160bdc56c5932f8d3a63de5d9407a1f4831190e
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-04-23 13:11:08 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[]args){
+        int x=10,y=5;
+        System.out.println(" Addition " + (x+y));
+        System.out.println(" Substraction "+(x-y));
+        System.out.println(" Multiplication "+(x*y));
+        System.out.println(" Division "+(x/y));
+    }
+
+    
+}
