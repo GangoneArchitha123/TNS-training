cca46a621f9b3d3d8d644c87e2d8b36e9d9eed42
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-05-02 22:26:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package tnsif;
+
+public class Main {
+	public static void main(String[]args) {
+		Polymorphism ob=new Polymorphism();
+		System.out.print("the addition of integer values"+ob.add(10, 20));
+		System.out.print("the addition of float values"+ob.add(1.2f,1.3f,1.7f));
+		System.out.print("the addition of double values"+ob.add(1.23,2.34));
+		
+		
+	}
+
+}
