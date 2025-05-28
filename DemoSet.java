2bb5ce73150aa846b12f881678420eeccb4663f6
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-05-28 16:08:21 +0530

    Create DemoSet.java

---
+++ DemoSet.java
@@ -0,0 +1,11 @@
+public class DemoSet {
+    public static void main(String[] args) {
+        Set<Integer> hs = new HashSet();
+        hs.add(10);
+        hs.add(12);
+        hs.add(30);
+        hs.add(10);
+        hs.add(3);
+        System.out.println(hs);
+    }
+}
