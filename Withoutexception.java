7864a53161ccc94b23bf134899c01d0aa9174ab5
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-06-06 17:00:11 +0530

    Create Withoutexception.java

---
+++ Withoutexception.java
@@ -0,0 +1,11 @@
+public class Withoutexception {
+	public static void main(String[] args) {
+      int a,b;
+      int result;
+      a=12;
+      b=0;
+      result=a/b;
+      System.out.println(result);
+	}
+}
+
