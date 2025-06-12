e633e2059c3a61b33539d7c8cf1ffc5c1c6b6166
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-06-12 19:12:05 +0530

    Create Comparable interface.java

---
+++ Comparable interface.java
@@ -0,0 +1,39 @@
+public class Student implements Comparable<Student>{
+	
+	public int sid;
+	public String name;
+	public int age;
+	//constructor
+	public Student(int sid, String name, int age) {
+		super();
+		this.sid = sid;
+		this.name = name;
+		this.age = age;
+	}
+	
+public int compareTo(Student st) {
+		if(age==st.age)
+		return 0;
+		else if(age>st.age)
+			return 1;
+		else
+			return -1;
+	}
+}
+public class StudentMain {
+	public static void main(String[] args) {
+		ArrayList<Student> al=new ArrayList<Student>();
+		al.add(new Student(101,"Joyce",23));
+		al.add(new Student(103,"Babu",22));
+		al.add(new Student(102,"Giva",18));
+		al.add(new Student(104,"Harsha",21));
+		al.add(new Student(105,"Beena",23));
+		Collections.sort(al);
+		for(Student st:al) {
+			System.out.println(st.sid+""+st.name+""+st.age);
+		}
+		
+		
+	}
+}
+
