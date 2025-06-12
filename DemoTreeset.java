af84a146aea0807ad4924926d0bf8a0732fb5268
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-06-12 19:07:19 +0530

    Create  DemoTreeset.java

---
+++ DemoTreeset.java
@@ -0,0 +1,13 @@
+public class DemoTreeset {
+	public static void main(String[] args) {
+SortedSet<String> ss=new TreeSet();
+	ss.add("harsha");
+	ss.add("Deepthi");
+	ss.add("Kiran");
+	ss.add("Monoj");
+	ss.add("Kiran");
+	for(String s:ss) {
+		System.out.println("The data"+s);
+	}
+	}
+}
