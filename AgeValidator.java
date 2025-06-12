6fd2dc67149b8737ff72a7d275225acf70a26bd9
Gangone Architha <143593226+GangoneArchitha123@users.noreply.github.com>
2025-06-12 19:02:08 +0530

    Update AgeValidator.java

--- AgeValidator.java
+++ AgeValidator.java
@@ -31,4 +31,21 @@ ublic class VoterInfo {
 		return "VoterInfo [name=" + name + ", age=" + age + "]";
 	}
 }
+public class Main {
+	public static void main(String[] args) {
+		StudentInfo obj=new StudentInfo();
+		obj.setRollNo(10);
+		obj.setName("Pooja");
+		obj.setPer(89);
+		obj.show();
+		
+		System.out.println("Enter Name and Age");
+		Scanner sc=new Scanner(System.in);
+		VoterInfo obj1=new VoterInfo();
+		obj1.setName(sc.next());
+		obj1.setAge(sc.nextInt());
+		if (obj1.getAge()!=0)
+			System.out.println(obj1);
+	}
+}
 
